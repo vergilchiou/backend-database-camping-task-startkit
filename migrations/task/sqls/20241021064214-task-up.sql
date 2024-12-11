755f28d7753a93630acb06dcddb08bdf19d73eab
vergilchiou <vergilchiou@gmail.com>
2024-12-11 15:10:56 +0800

    add 5-1-2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -199,6 +199,14 @@ VALUES
         -- 1. 預約人設為`王小明`
         -- 2. 預約時間`booking_at` 設為2024-11-24 16:00:00
         -- 3. 狀態`status` 設定為即將授課
+INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status)
+VALUES
+(
+    (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
+    (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io') AND name = '重訓基礎課'),
+    '2024-11-24 16:00:00',
+    '即將授課'
+);
     -- 2. 新增： `好野人` 預約 `李燕容` 的課程
         -- 1. 預約人設為 `好野人`
         -- 2. 預約時間`booking_at` 設為2024-11-24 16:00:00
@@ -206,7 +214,7 @@ VALUES
 INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status)
 VALUES
 (
-    (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
+    (SELECT id FROM "USER" WHERE email = 'richman@hexschooltest.io'),
     (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io') AND name = '重訓基礎課'),
     '2024-11-24 16:00:00',
     '即將授課'
