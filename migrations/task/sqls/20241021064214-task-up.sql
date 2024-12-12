e7f94d016297e322ba06844a354e421d08bd5ea6
vergilchiou <vergilchiou@gmail.com>
2024-12-12 11:09:37 +0800

    test just to 5-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -231,14 +231,14 @@ AND course_id = (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" 
     -- 1. 預約人設為`王小明`
     -- 2. 預約時間`booking_at` 設為2024-11-24 17:10:25
     -- 3. 狀態`status` 設定為即將授課
--- INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status)
--- VALUES
--- (
---     (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
---     (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io' AND name = '重訓基礎課')),
---     '2024-11-24 17:10:25',
---     '即將授課'
--- );
+INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status)
+VALUES
+(
+    (SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
+    (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io' AND name = '重訓基礎課')),
+    '2024-11-24 17:10:25',
+    '即將授課'
+);
 
 
 -- 5-4. 查詢：取得王小明所有的預約紀錄，包含取消預約的紀錄
